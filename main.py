fc27ff8dbe9c369689daa1b0dfc44aebe96f067d
kritical1326 <73088880+kritical1326@users.noreply.github.com>
2022-02-06 19:55:17 +0530

    ✨Make, add and post to a db

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from typing import Optional
-
+from decouple import config
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import time
+from pymongo import MongoClient
+    
+mongo_url = config('MONGO')
+print(mongo_url)
+
+def get_database():
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    client = MongoClient(mongo_url,tls=True, tlsAllowInvalidCertificates=True)   
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client['student']
+
+dbname = get_database()
+collection_name= dbname["student"]
 
 app = FastAPI()
 
@@ -24,12 +38,15 @@ def healthCheck():
 
 @app.get("/student")
 async def readItem(name: str):
-    return {"name": name}
+    result = collection_name.find_one({"name": name})
+    if result is None:
+        return{"result": "failed","status": 404}
+    return {"name": result["name"],"registration number": result["registration_number"], "division": result["division"], "FA name": result["fa_name"]}
 
 @app.post("/student")
 async def create_item(info : Student):
-    # req_info = await info.json()
-    # print(req_info["name"])
+    contojson = info.json()
+    collection_name.insert_one(eval(contojson))
     return {
         "status" : "SUCCESS",
         "data" : info
@@ -37,17 +54,13 @@ async def create_item(info : Student):
     
 @app.put("/student")
 async def updateInformation(info : Student):
-    # req_info = await info.json()
-    # print(req_info["name"])
     return {
         "status" : "SUCCESS",
         "data" : info
     }
 
 @app.delete("/student")
-async def updateInformation(info : StudentDelete):
-    # req_info = await info.json()
-    # print(req_info["name"])
+async def deleteInformation(info : StudentDelete):
     return {
         "status" : "SUCCESS",
         "data" : info
